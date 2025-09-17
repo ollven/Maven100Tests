f595bd1bb60ef6ee54a9f3793d9e7a0c120225b5
Olga Sventukh <ollven@yandex.ru>
2025-09-17 17:05:55 +0200

    Delete src/test/java/com/example/tests/SingleTest07.java

--- src/test/java/com/example/tests/SingleTest07.java
+++
@@ -1,9 +0,0 @@
-package com.example.tests;
-
-public class SingleTest07 {
-    @org.junit.jupiter.api.Test
-    void test_01() throws InterruptedException {
-        Thread.sleep(1000);
-        org.junit.jupiter.api.Assertions.assertTrue(true);
-    }
-}
